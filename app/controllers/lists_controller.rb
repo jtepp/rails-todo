c6d364d5d7f3be6bf1ee980e02054b0ba54a6d53
Jacob Tepperman <jtepp@icloud.com>
2024-05-03 17:40:56 -0400

    added show complete functionality to list show page, and fixed non-empty list deleting

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,6 +22,12 @@ class ListsController < ActionController::Base
 
     def delete
         @list = List.find(params[:id])
+        if @list.tasks.length > 0
+            @list.tasks.each do |t|
+                t.delete
+            end
+        end
+
         if @list.delete
             redirect_to root_path
         else
