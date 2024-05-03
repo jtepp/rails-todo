6ceed3a87021f4ca18d9b23ad49863de54c202c9
Jacob Tepperman <jtepp@icloud.com>
2024-05-02 23:47:17 -0400

    generated, migrated List and Task models

---
+++ db/migrate/20240503034656_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks do |t|
+      t.references :list_id, null: false, foreign_key: true
+      t.string :label
+      t.text :notes
+      t.integer :status
+      t.date :due
+
+      t.timestamps
+    end
+  end
+end
