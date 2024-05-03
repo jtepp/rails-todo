b7df4ffcc4e1e6fc890808edf227f48d327c0271
Jacob Tepperman <jtepp@icloud.com>
2024-05-03 01:01:41 -0400

    set up fun new list form without an action yet

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,8 @@
 class ListsController < ActionController::Base
+    layout "application"
     def index
         @lists = List.all
+        @new_list = List.new
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
  
+  post "/new_list", to: "lists#create"
   root "lists#index"
 end
