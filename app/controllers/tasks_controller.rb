ba0641ff08b09978824dde36c94c9e79b98dc46a
Jacob Tepperman <jtepp@icloud.com>
2024-05-03 14:18:42 -0400

    added new task form but not the actions yet

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,5 @@
+class TasksController < ActionController::Base
+    def create
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
  
+  post "/tasks/new", to: "tasks#create", as: :new_task
+
   post "/lists/new", to: "lists#create", as: :new_list
   get "/lists/:id", to: "lists#show", as: :list
   delete "/lists/:id", to: "lists#delete"
+
+
   root "lists#index"
 end
